3669365a35587b7f47f99c418e813eec15c064f2
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2022-09-18 07:32:51 +0530

    Time: 45 ms (7.98%), Space: 20.3 MB (43.13%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
+        stack<int>st;
         int n=arr.size();
-        int mxl[n],mxr[n];
-        mxl[0]=arr[0];
-        for(int i=1;i<n;i++)
+        int mn[n],mx[n];
+        mn[0]=arr[0];
+        for(int i=1;i<arr.size();i++)
         {
-            mxl[i]=max(mxl[i-1],arr[i]);
+            mn[i]=max(mn[i-1],arr[i]);
         }
-        mxr[n-1]=arr[n-1];
+        mx[n-1]=arr[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            mxr[i]=max(mxr[i+1],arr[i]);
+            mx[i]=max(mx[i+1],arr[i]);
         }
-        int water[n],sum=0;
+        int water[n];
+        int sum=0;
         for(int i=0;i<n;i++)
         {
-            water[i]=(min(mxr[i],mxl[i])-arr[i]);
+            water[i]=min(mn[i],mx[i])-arr[i];
             sum+=water[i];
         }return sum;
     }
