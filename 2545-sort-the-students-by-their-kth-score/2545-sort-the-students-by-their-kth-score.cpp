8ca1e5ea6d9e35850b0a5cb2f2cded07880e869d
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-01-23 02:06:31 +0800

    Time: 74 ms (92.31%), Space: 27.5 MB (15.38%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        vector<pair<int,int>>v;
-        for(int i=0;i<score.size();i++)
-        {
-            v.push_back(make_pair(score[i][k],i));
-        }sort(v.begin(),v.end());
-        reverse(v.begin(),v.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<score.size();i++)
-        {
-            int kk=v[i].second;
-            ans.push_back(score[kk]);
-        }
-        return ans;
+     vector<vector<int>> sortTheStudents(vector<vector<int>>& A, int k) {
+        sort(A.begin(), A.end(), [&](auto const & a, auto const & b) {
+            return a[k] > b[k];
+        });
+        return A;
     }
 };
