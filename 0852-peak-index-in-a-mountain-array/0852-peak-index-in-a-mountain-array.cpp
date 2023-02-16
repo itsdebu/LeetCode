c29d655d752adfd4ac384831412700d44193935e
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-02-17 07:44:04 +0800

    Time: 111 ms (91.39%), Space: 59.6 MB (94.98%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int bs(vector<int>&arr,int i,int j)
-    {
-
-        if(i<j){
+    int peakIndexInMountainArray(vector<int>& arr) {
+        //binary aearch o(logn)
+        int i=0,j=arr.size()-1;
+        while(i<j)
+        {
             int mid=i+(j-i)/2;
             if(arr[mid]<arr[mid+1])
-                return bs(arr,mid+1,j);
-            else return bs(arr,i,mid);
+            {
+                i=mid+1;
+            }
+            else j=mid;
         }return i;
-    }
-    int peakIndexInMountainArray(vector<int>& arr) {
-        return bs(arr,0,arr.size()-1);
     }
 };
