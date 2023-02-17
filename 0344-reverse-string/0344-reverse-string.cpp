c9310e0e285d35777e8a8df72237becdb3896168
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-02-17 08:27:53 +0800

    Time: 27 ms (43.78%), Space: 23.2 MB (80.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    void rev(vector<char>&s,int i,int j)
-    {
-        if(i>j)return;
-        swap(s[i],s[j]);
-        i++,j--;
-        rev(s,i,j);
-    }
     void reverseString(vector<char>& s) {
-        return rev(s,0,s.size()-1);
+       return reverse(s.begin(),s.end());
     }
 };
