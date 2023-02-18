eb7281c1685776fd2c7c09d56d3eedec2bbdb7c5
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-02-18 14:10:01 +0800

    Time: 20 ms (81.15%), Space: 25.4 MB (52.32%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,25 +11,29 @@
  */
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    TreeNode* solve(TreeNode* root)
-    {
-        if(root==NULL)return NULL;
-        mp[root->val]++;
-        solve(root->left);
-        solve(root->right);
-        return NULL;
-    }
-    vector<int> findMode(TreeNode* root) {
-        vector<int>v;
-        solve(root);
-        int mx=INT_MIN;
-        for (auto i : mp) {
-            mx=max(mx,i.second);
-        }
-        for(auto i:mp)
-        {
-            if(mx==i.second)v.push_back(i.first);
-        }return v;
-    }
+	//Initializing unordered map universally so tha we can use it anywhere also here we are using hashmap to store the frequency of elements in BST.
+	unordered_map<int,int>mp;
+	TreeNode* solve(TreeNode* root)
+	{
+		//Here we are using dfs to store elements in map in preoreder traversal;
+		if(root==NULL)return NULL;
+		mp[root->val]++;
+		solve(root->left);
+		solve(root->right);
+		return NULL;
+	}
+	vector<int> findMode(TreeNode* root) {
+		//initialize a vector to store the maximum frequent elements 
+		vector<int>v;
+		solve(root);//calling function solve to store elements in hashmap
+		int mx=INT_MIN;
+		for (auto i : mp) {
+			mx=max(mx,i.second);//here we are finding maximum frequecy in BST of element.
+		}
+		for(auto i:mp)
+		{
+			if(mx==i.second)v.push_back(i.first);//here we are comparing the max frequency to every element if it does then push it in vector;
+		}return v;//and then return it
+	}
 };
+// Plss Upvote If You Found This Helpful
