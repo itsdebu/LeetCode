f1a918e3c926c484b72c5b567b1e8c74244cbe0a
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-04 01:27:15 +0800

    Time: 3 ms (92.73%), Space: 9 MB (42.21%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,29 +1,62 @@
 class Solution {
 public:
-    int compress(vector<char>& chars) {
-         int writeIndex = 0, currentCount = 1;
+int compress(vector<char>& v) {
+        
+        int cnt = 1;
 
-        char prev = chars[0];
+        char prev = v[0];
 
-        for(int i=1 ; i<=chars.size() ; i++) {
-            if(i < chars.size() && prev == chars[i]) {
-                currentCount++;
-            }
-            else {
-                chars[writeIndex++] = prev;
-                if(currentCount > 1) {
-                    string digits = to_string(currentCount);
+        int n = v.size();
 
-                    for(int j=0 ; j<digits.size() ; j++) {
-                        chars[writeIndex++] = digits[j];
+        vector<char>rejult;
+
+        for(int i = 1 ;i < n ; i++){
+            if(v[i] == prev){
+                cnt++;
+            }else{
+                
+
+                if(cnt == 1){
+                    rejult.push_back(prev);
+                }
+                else if(cnt<=9){
+                    rejult.push_back(prev);
+                    char ch = '0'+cnt;
+                    rejult.push_back(ch);
+                }
+                else{
+                    rejult.push_back(prev);
+                    string temp = to_string(cnt);
+                    int j = 0;
+                    while(j<temp.size()){
+                        rejult.push_back(temp[j++]);
                     }
                 }
-
-                currentCount = 1;
-                if(i < chars.size()) prev = chars[i];
+                
+                prev=v[i];
+                cnt=1;
+            }
+        }
+        if(cnt == 1 ){
+            rejult.push_back(prev);
+        }
+        else if(cnt<=9){
+            rejult.push_back(prev);
+            char ch = '0'+cnt;
+            rejult.push_back(ch);
+        }
+        else{
+            rejult.push_back(prev);
+            string temp = to_string(cnt);
+            int j = 0;
+            while(j<temp.size()){
+                rejult.push_back(temp[j++]);
             }
         }
 
-        return writeIndex;
+
+        v=rejult;
+      
+        return rejult.size();
     }
 };
