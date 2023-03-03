f6fa6db7c463200001c595f422e9f674d23cbf88
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-04 01:26:50 +0800

    Time: 5 ms (68.77%), Space: 9.1 MB (42.21%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    vector<char>char_array(int n)
-    {
-        vector<char>v;
-        while(n!=0)
-        {
-            v.push_back((n%10)+ '0');
-            n/=10;
-        }
-        reverse(v.begin(),v.end());
-        return v;
-    }
     int compress(vector<char>& chars) {
-        if(chars.size()==1)return 1;
-        char curr_char=chars[0];
-        int freq_count=0;
-        int write_index=0;
-        for(int i=0;i<=chars.size();i++)
-        {
-            if(i!=chars.size() and chars[i]==curr_char)freq_count++;
-            else
-            {
-                chars[write_index++]=curr_char;
-                if(freq_count!=1)
-                {
-                    vector<char>frq_arr=char_array(freq_count);
-                    for(auto number:frq_arr)
-                    {
-                        chars[write_index++]=number;
+         int writeIndex = 0, currentCount = 1;
+
+        char prev = chars[0];
+
+        for(int i=1 ; i<=chars.size() ; i++) {
+            if(i < chars.size() && prev == chars[i]) {
+                currentCount++;
+            }
+            else {
+                chars[writeIndex++] = prev;
+                if(currentCount > 1) {
+                    string digits = to_string(currentCount);
+
+                    for(int j=0 ; j<digits.size() ; j++) {
+                        chars[writeIndex++] = digits[j];
                     }
                 }
-                if(i!=chars.size())curr_char=chars[i];
-                freq_count=1;
+
+                currentCount = 1;
+                if(i < chars.size()) prev = chars[i];
             }
-        }return write_index;
+        }
+
+        return writeIndex;
     }
 };
