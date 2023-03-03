5089bf313525b237b0b5c7e6f43836d28ccdae61
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-04 01:21:44 +0800

    Time: 10 ms (26.70%), Space: 9.1 MB (6.90%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-int compress(vector<char>& chars) {
-	int i = 0;
-	for(int j = 1, count = 1; j <= chars.size(); j++, count++) {
-		if(j == chars.size() || chars[j] != chars[j - 1]) {
-			chars[i++] = chars[j - 1];  
-			if(count >= 2) 
-				for(char digit : to_string(count)) 
-					chars[i++] = digit;
-			count = 0;
-		}
-	}
-	return i;
-}  
-    
+    vector<char>char_array(int n)
+    {
+        vector<char>v;
+        while(n!=0)
+        {
+            v.push_back((n%10)+ '0');
+            n/=10;
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+    int compress(vector<char>& chars) {
+        if(chars.size()==1)return 1;
+        char curr_char=chars[0];
+        int freq_count=0;
+        int write_index=0;
+        for(int i=0;i<=chars.size();i++)
+        {
+            if(i!=chars.size() and chars[i]==curr_char)freq_count++;
+            else
+            {
+                chars[write_index++]=curr_char;
+                if(freq_count!=1)
+                {
+                    vector<char>frq_arr=char_array(freq_count);
+                    for(auto number:frq_arr)
+                    {
+                        chars[write_index++]=number;
+                    }
+                }
+                if(i!=chars.size())curr_char=chars[i];
+                freq_count=1;
+            }
+        }return write_index;
+    }
 };
