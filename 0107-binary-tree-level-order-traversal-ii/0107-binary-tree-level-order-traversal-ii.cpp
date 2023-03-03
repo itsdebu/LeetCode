8fb3ea259ccae45484bc41a5164ab268a1531c46
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-04 02:18:31 +0800

    Time: 8 ms (34.60%), Space: 13.4 MB (9.15%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -11,18 +11,24 @@
  */
 class Solution {
 public:
+    int height(TreeNode* root)
+    {
+        if(!root)return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
     void helper(TreeNode* root,vector<vector<int>>&ans,int index)
     {
         if(!root)return;
         if(index==ans.size())ans.push_back({});
         ans[index].push_back(root->val);
-        helper(root->left,ans,index+1);
-        helper(root->right,ans,index+1);
+        helper(root->left,ans,index-1);
+        helper(root->right,ans,index-1);
     }
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>>ans;
-        helper(root,ans,0);
-        reverse(ans.begin(),ans.end());
+        int index=height(root);
+        vector<vector<int>>ans(index);
+        index--;
+        helper(root,ans,index);
         return ans;
     }
 };
