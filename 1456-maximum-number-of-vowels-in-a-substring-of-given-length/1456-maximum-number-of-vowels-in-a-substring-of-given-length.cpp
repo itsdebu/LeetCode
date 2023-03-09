7e1f640d9df17270b1a30ec71c0563056e20391f
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-10 00:54:59 +0800

    Time: 15 ms (96.59%), Space: 10 MB (28.90%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int count=0,mx=0;
-        int i=0,j=0;
-        if(s.size()>k)
-        {
-            for(int i=0;i<k;i++)
-            {
-                if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u')count++;
-            }
-            mx=count;
-            i=1,j=1;
-            while(j<s.size())
-            {
-                if(j-i+1==k)
-                {
-                    if(s[i-1]=='a' or s[i-1]=='e' or s[i-1]=='i' or s[i-1]=='o' or s[i-1]=='u')count--;
-                    if(s[j]=='a' or s[j]=='e' or s[j]=='i' or s[j]=='o' or s[j]=='u')count++;
-                    cout<<count<<endl;
-                    mx=max(mx,count);
-                    i++,j++;
-                }
-                else {
-                    j++;
-                }
-            }
+        int count = 0, len = s.length(), Max = 0;
+        for(int i=0;i<k;i++)    
+            if(isVowel(s[i])) 
+               count++;
+        Max = max(Max, count);
+        for(int i=k;i<len;i++){
+            if(isVowel(s[i])) count++;
+            if(isVowel(s[i-k])) count--;
+            Max = max(Max, count);
         }
-        else
-        {
-            for(int i=0;i<k;i++)
-            {
-                if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u')count++;
-            }
-            mx=count;
-        }
-        return mx;
+        return Max;
+    }
+    bool isVowel(char c){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') return true;
+        return false;
     }
 };
