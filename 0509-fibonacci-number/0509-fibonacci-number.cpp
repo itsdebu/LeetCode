43d3dbff1caf8fc0f751b21e4fc238f1d557be32
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-15 00:31:44 +0800

    Time: 0 ms (100.00%), Space: 6 MB (21.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int>dp(n+1,-1);
+        int prev=1;
+        int sec_prev=0;
         if(n<=1)return n;
-        dp[0]=0;
-        dp[1]=1;
         for(int i=2;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+            int ans=prev+sec_prev;
+            sec_prev=prev;
+            prev=ans;
+        }return prev;
     }    
 };
