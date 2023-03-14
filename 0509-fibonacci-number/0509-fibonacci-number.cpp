116a5fe778988dbfec139ab6eeb09ec3be9e3b11
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-15 00:25:43 +0800

    Time: 12 ms (45.38%), Space: 5.8 MB (93.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     int fib(int n) {
         if(n<=1)return n;
-        int s=0,s1=1,ans=0;
-        for(int i=2;i<=n;i++)
-        {
-            ans=s+s1;
-            s=s1;
-            s1=ans;
-        }return ans;
+        return fib(n-1)+fib(n-2);
     }
 };
