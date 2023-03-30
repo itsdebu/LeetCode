edd5fdd313088df482f5555ec89d629e71794d5e
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-03-30 15:12:23 +0800

    Time: 3 ms (94.44%), Space: 8 MB (98.27%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
- int maxSatisfaction(vector<int>& sat) {
-        sort(sat.begin(),sat.end());     // sort no matter the place of elements
-        reverse(sat.begin(), sat.end()); // make it to non increasing 
-        int n=sat.size();
-        vector<int>sum;
-        sum = sat;                      // we need a vector for storing prefix sum
-        for(int i=1;i<n;i++){
-            sum[i]+=sum[i-1];              // storing prefix sum in sum array
+    int maxSatisfaction(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        for(int i=1;i<nums.size();i++)
+        {
+            nums[i]=nums[i]+nums[i-1];
         }
-        int mx=0,cs=0;              // intislise maximum possible sum and current possible sum
-        for(int i=0;i<n;i++){
-            cs+=sum[i];             // storing in current sum 
-            mx=max(cs,mx);             // comparing 
-        }
-        return mx;
+        int sum=0,mx=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            mx=max(mx,sum);
+        }return mx;
     }
 };
