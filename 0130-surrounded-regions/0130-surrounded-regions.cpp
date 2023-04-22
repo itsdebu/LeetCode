758e1f142889b1d6394c2ac39d8357dc97596eac
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-04-22 15:10:37 +0800

    Time: 15 ms (50.62%), Space: 10.4 MB (35.05%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -22,15 +22,27 @@ public:
         vector<vector<int>>vis(n,vector<int>(m,0));
         int delrow[4]={-1,0,1,0};
         int delcol[4]={0,1,0,-1};
+        for(int i=0;i<m;i++)
+        {
+            if(board[0][i]=='O' and !vis[0][i])
+            {
+                dfs(0,i,vis,board,delrow,delcol);
+            }
+            if(board[n-1][i]=='O' and !vis[n-1][i])
+            {
+                dfs(n-1,i,vis,board,delrow,delcol);
+            }
+        }
         for(int i=0;i<n;i++)
         {
-            if(!vis[i][0] and board[i][0]=='O')dfs(i,0,vis,board,delrow,delcol);
-            if(!vis[i][m-1] and board[i][m-1]=='O')dfs(i,m-1,vis,board,delrow,delcol);
-        }
-        for(int j=0;j<m;j++)
-        {
-            if(!vis[0][j] and board[0][j]=='O')dfs(0,j,vis,board,delrow,delcol);
-            if(!vis[n-1][j] and board[n-1][j]=='O')dfs(n-1,j,vis,board,delrow,delcol);
+            if(board[i][0]=='O' and !vis[i][0])
+            {
+                dfs(i,0,vis,board,delrow,delcol);
+            }
+            if(board[i][m-1]=='O' and !vis[i][m-1])
+            {
+                dfs(i,m-1,vis,board,delrow,delcol);
+            }
         }
         for(int i=0;i<n;i++)
         {
