dbfb079868bc3d8c3ae0beb06fbf2e01c8e73dc5
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-04-22 12:34:54 +0800

    Time: 30 ms (95.85%), Space: 12.4 MB (54.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>nums,int delrow[],int delcol[])
+    void dfs(int row,int col,vector<vector<char>>&nums,int m,int n)
     {
-        int n=nums.size();
-        int m=nums[0].size();
-        vis[row][col]=1;
-        queue<pair<int,int>>q;
-        q.push({row,col});
-        while(!q.empty())
-        {
-            int new_row=q.front().first;
-            int new_col=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
-            {
-                int next_row=new_row+delrow[i];
-                int next_col=new_col+delcol[i];
-                if(next_row>=0 and next_row<n and next_col>=0 and next_col<m and !vis[next_row][next_col] and nums[next_row][next_col]=='1')
-                {
-                    q.push({next_row,next_col});
-                    vis[next_row][next_col]=1;
-                }
-            }
-        }
+        if(row<0 or row>=n or col<0 or col>=m or nums[row][col]=='0')return;
+        nums[row][col]='0';
+        dfs(row+1,col,nums,m,n);
+        dfs(row,col+1,nums,m,n);
+        dfs(row-1,col,nums,m,n);
+        dfs(row,col-1,nums,m,n);
+        return;
     }
     int numIslands(vector<vector<char>>& nums) {
         int n=nums.size();
         int m=nums[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        int delrow[4]={-1,0,1,0};
-        int delcol[4]={0,1,0,-1};
         int count=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] and nums[i][j]=='1')
+                if(nums[i][j]=='1')
                 {
                     count++;
-                    dfs(i,j,vis,nums,delrow,delcol);
+                    dfs(i,j,nums,m,n);
                 }
             }
         }return count;
