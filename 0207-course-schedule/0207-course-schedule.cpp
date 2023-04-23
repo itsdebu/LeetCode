0d59cb1b070ef2c69897fba6d034cfca86a3ab41
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-04-24 03:02:16 +0800

    Time: 24 ms (59.80%), Space: 13.6 MB (73.71%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& grid) {
+        vector<int>adj[n];
+        for(auto i:grid)
+        {
+            adj[i[0]].push_back(i[1]);
+        }
+        vector<int>Indegree(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+                Indegree[it]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(Indegree[i]==0)q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node])
+            {
+                Indegree[it]--;
+                if(Indegree[it]==0)q.push(it);
+            }
+        }
+        if(ans.size()==n)return true;
+        return false;
+    }
+};
