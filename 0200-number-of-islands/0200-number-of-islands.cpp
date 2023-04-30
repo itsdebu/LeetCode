b2f5ba1157e89d0ea60d052d252f86a3ed6f7faf
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-04-30 15:01:53 +0800

    Time: 710 ms (5.01%), Space: 396.2 MB (5.00%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<char>>&nums,int m,int n)
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>grid,int deltarow[],int deltacol[])
     {
-        if(row<0 or row>=n or col<0 or col>=m or nums[row][col]=='0')return;
-        nums[row][col]='0';
-        dfs(row+1,col,nums,m,n);
-        dfs(row,col+1,nums,m,n);
-        dfs(row-1,col,nums,m,n);
-        dfs(row,col-1,nums,m,n);
-        return;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        while(!q.empty())
+        {
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int new_row=r+deltarow[i];
+                int new_col=c+deltacol[i];
+                if(new_row>=0 and new_row<n and new_col>=0 and new_col<m and !vis[new_row][new_col] and grid[new_row][new_col]=='1')
+                {
+                    q.push({new_row,new_col});
+                    vis[new_row][new_col]=1;
+                }   
+            }
+        }
     }
-    int numIslands(vector<vector<char>>& nums) {
-        int n=nums.size();
-        int m=nums[0].size();
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int deltarow[4]={-1,0,1,0};
+        int deltacol[4]={0,1,0,-1};
         int count=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(nums[i][j]=='1')
+                if(!vis[i][j] and grid[i][j]=='1')
                 {
                     count++;
-                    dfs(i,j,nums,m,n);
+                    dfs(i,j,vis,grid,deltarow,deltacol);
                 }
             }
         }return count;
