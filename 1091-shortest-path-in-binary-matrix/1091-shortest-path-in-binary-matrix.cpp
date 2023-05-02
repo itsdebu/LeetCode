d977e2b18fc620014407aa0678ad7817e548a82e
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-03 04:03:30 +0800

    Time: 83 ms (40.96%), Space: 21.6 MB (55.33%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int res = 1;
-        int row = grid.size();
-        if (row == 0) return -1;
-        int col = grid[0].size();
-        if (col == 0 ) return -1;
-        if (grid[0][0] != 0 | grid[row-1][col-1] != 0) return -1;
-
-            queue<pair<int, int>> queue;
-    queue.push(make_pair(0,0));
-    vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
-    grid[0][0] = 1;
-    while(!queue.empty()){
-        auto curr = queue.front();
-        int x = curr.first, y = curr.second;
-        if( x == row -1 && y == col -1) return grid[x][y];
-        
-        for(auto direction : directions){
-            int nx = x + direction[0];
-            int ny = y + direction[1];
-            if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                queue.push(make_pair(nx,ny));
-                grid[nx][ny] = grid[x][y] + 1;
+        if(grid[0][0]==1)return -1;
+        if(grid.size()==1)return 1;
+        int n=grid.size();
+        pair<int,int>source={0,0},destination={n-1,n-1};
+        if(source==destination)return 0;
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        q.push({{0,0},0});
+        dist[0][0]=0;
+        while(!q.empty())
+        {
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+            q.pop();
+            if(row==n-1 and col==n-1)return steps+1;
+            for(int delrow=-1;delrow<=1;delrow++)
+            {
+                for(int delcol=-1;delcol<=1;delcol++)
+                {
+                    int new_row=row+delrow;
+                    int new_col=col+delcol;
+                    if(new_row>=0 and new_row<n and new_col>=0 and new_col<n and grid[new_row][new_col]==0)
+                    {
+                        if(dist[row][col]+1<dist[new_row][new_col])
+                        {
+                            dist[new_row][new_col]=1+dist[row][col];
+                            q.push({{new_row,new_col},steps+1});
+                        }
+                    }
+                }
             }
-        }
-        queue.pop();
-    }
-    return -1;
-        
+        }return -1;
     }
 };
