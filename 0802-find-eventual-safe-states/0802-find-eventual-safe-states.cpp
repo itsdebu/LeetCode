16c11f4edfff770e74eb74ae955a5abb8832450c
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-02 12:33:31 +0800

    Time: 230 ms (16.62%), Space: 61.6 MB (21.16%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>Indegree(n);
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++)
+        int V=graph.size();
+        vector<int>indegree(V,0);
+        vector<vector<int>>adj(V);
+        for(int i=0;i<V;i++)
         {
             for(auto it:graph[i])
             {
                 adj[it].push_back(i);
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(auto it:adj[i])
-            {
-                Indegree[it]++;
-            }
-        }
-        queue<int>q;
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            if(Indegree[i]==0)
-            {
-                q.push(i);
-            }
-        }
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto it:adj[node])
-            {
-                Indegree[it]--;
-                if(Indegree[it]==0)q.push(it);
-            }
-        }
+	    queue<int>q;
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it:adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    for(int i=0;i<V;i++)
+	    {
+	        if(indegree[i]==0)q.push(i);
+	    }
+	    vector<int>ans;
+	    while(!q.empty())
+	    {
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto it:adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)q.push(it);
+	        }
+	    }
         sort(ans.begin(),ans.end());
         return ans;
     }
