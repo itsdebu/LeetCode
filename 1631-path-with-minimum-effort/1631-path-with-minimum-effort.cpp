a910e3b624c28c374047825e0c329b5f8ce44bb8
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-03 04:26:56 +0800

    Time: 303 ms (24.20%), Space: 16.9 MB (98.44%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -4,16 +4,16 @@ public:
         int n=heights.size();
         int m=heights[0].size();
         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        priority_queue<pair<int, pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int, pair<int,int>>>> q;
-        q.push({0,{0,0}});
+        priority_queue<pair<pair<int,int>,int>, vector<pair<pair<int,int>,int>>, greater<pair< pair<int,int>,int>>> q;
+        q.push({{0,0},0});
         dist[0][0]=0;
         int delrow[4]={-1,0,1,0};
         int delcol[4]={0,1,0,-1};
         while(!q.empty())
         {
-            int dis=q.top().first;
-            int row=q.top().second.first;
-            int col=q.top().second.second;
+            int dis=q.top().second;
+            int row=q.top().first.first;
+            int col=q.top().first.second;
             q.pop();
             if(row==n-1 and col==m-1)return dis;
             for(int i=0;i<4;i++)
@@ -23,7 +23,7 @@ public:
                 if(new_row>=0 and new_row<n and new_col>=0 and new_col<m and dist[new_row][new_col]>max(abs(heights[row][col]-heights[new_row][new_col]),dis))
                 {
                     dist[new_row][new_col]=max(abs(heights[row][col]-heights[new_row][new_col]),dis);
-                    q.push({dist[new_row][new_col],{new_row,new_col}});
+                    q.push({{new_row,new_col},dist[new_row][new_col]});
                 }
             }
         }return -1;
