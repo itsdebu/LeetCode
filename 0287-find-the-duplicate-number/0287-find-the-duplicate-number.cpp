9dd8dc737e6e5adc723d65628f9821d1d8384f92
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-05 03:22:01 +0800

    Time: 155 ms (39.58%), Space: 61.1 MB (92.86%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>freq(n,0);
-        for(int i=0;i<nums.size();i++)freq[nums[i]]++;
-        for(int i=0;freq.size();i++)
+        int low = 1, high = nums.size() - 1, cnt;
+        
+        while(low <=  high)
         {
-            if(freq[i]>1)return i;
-        }return 0;
+            int mid = low + (high - low) / 2;
+            cnt = 0;
+            // cnt number less than equal to mid
+            for(int n : nums)
+            {
+                if(n <= mid)
+                    ++cnt;
+            }
+            // binary search on left
+            if(cnt <= mid)
+                low = mid + 1;
+            else
+            // binary search on right
+                high = mid - 1;
+            
+        }
+        return low;
     }
 };
