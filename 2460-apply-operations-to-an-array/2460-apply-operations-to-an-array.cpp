96fa4d74dfb7fa730afc00a63fb4f0d7a3f8d064
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-09 11:46:15 +0800

    Time: 10 ms (8.87%), Space: 8.9 MB (33.36%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        vector<int>v;
+        vector<int>ans;
+        int zero_count=0;
         for(int i=0;i<nums.size()-1;i++)
         {
-            if(nums[i]==nums[i+1])
-            {
-                nums[i]=nums[i]*2;
+            if(nums[i]==nums[i+1] and nums[i]!=0){
+                ans.push_back(nums[i]*2);
                 nums[i+1]=0;
             }
-            else
-                continue;
+            else if(nums[i]!=nums[i+1] and nums[i]!=0) {
+                ans.push_back(nums[i]);
+                            }
+            if(nums[i]==0)zero_count++;
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0)
-            {
-                v.push_back(nums[i]);
-            }
-        }
-        int c=nums.size()-v.size();
-        while(c--)
-        {
-            v.push_back(0);
-        }return v;
+        if(nums[nums.size()-1]==0)zero_count++;
+        else if(nums[nums.size()-1]!=0)ans.push_back(nums[nums.size()-1]);
+        while(zero_count--)ans.push_back(0);
+        return ans;
     }
 };
