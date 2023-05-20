8787265d8c085dd0f017f60edbe41d1703e33425
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-20 13:05:00 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (37.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n<=3)return n;
-        int prev=2;
-        int sec_prev=1;
+        vector<int>dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
         for(int i=3;i<=n;i++)
         {
-            int ans=prev+sec_prev;
-            sec_prev=prev;
-            prev=ans;
-        }return prev;
+            dp[i]=dp[i-1]+dp[i-2];
+        }return dp[n];
     }
 };
