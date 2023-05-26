7d5a071d7036c4f0ba83587fc540247cb0661129
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-05-26 12:13:10 +0800

    Time: 55 ms (72.11%), Space: 10.9 MB (69.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,24 +1,22 @@
 class Solution {
-public:    
-    int dp[1000 + 1][1000 + 1];
-    
-    int maxLen(string& s1, string& s2, int n, int m)
+public:
+    int dp[1000+1][1000+1];
+    int max_common(int index1,int index2,string &str1,string &str2)
     {
-        if (n == 0 || m == 0)
-            return 0;
-        
-        if (dp[n][m] != -1)
-            return dp[n][m];
-
-        if (s1[n - 1] == s2[m - 1])
-            return dp[n][m] = 1 + maxLen(s1, s2, n - 1, m - 1);
+        if(index1==0 or index2==0)return 0;
+        if(dp[index1][index2]!=-1)return dp[index1][index2];
+        if(str1[index1-1]==str2[index2-1])
+        {
+            return dp[index1][index2]=1+max_common(index1-1,index2-1,str1,str2);
+        }
         else
-            return dp[n][m] = max(0 + maxLen(s1, s2, n - 0, m - 1), 0 + maxLen(s1, s2, n - 1, m - 0));
+        {
+            return dp[index1][index2]=max(0+max_common(index1-1,index2,str1,str2),max_common(0+index1,index2-1,str1,str2));
+        }
     }
-        
-    int longestCommonSubsequence(string s1, string s2) 
-    {
-        memset(dp, -1, sizeof(dp));        
-        return maxLen(s1, s2, s1.length(), s2.length());
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size(),n1=text2.size();
+        memset(dp,-1,sizeof(dp));
+        return max_common(n,n1,text1,text2);
     }
 };
