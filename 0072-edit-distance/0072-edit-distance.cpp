9852c7af62a48f20a5ceab3cbcd0658dc1db4b1a
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-01 14:16:29 +0800

    Time: 100 ms (8.94%), Space: 7.5 MB (77.80%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,20 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();//first word length
-    const int n = word2.length();//second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[m][n];
-  }
+public:
+    int dp[501][501];
+    int min_operations(string &s1,string &s2,int i,int j)
+    {
+        if(i<0)return j+1;
+        if(j<0)return i+1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1[i]==s2[j])
+        {
+            return dp[i][j]=min_operations(s1,s2,i-1,j-1);
+        }
+        return dp[i][j]=min({1+min_operations(s1,s2,i-1,j),1+min_operations(s1,s2,i,j-1),1+min_operations(s1,s2,i-1,j-1)});
+    }
+    int minDistance(string word1, string word2) {
+        int n=word1.size(),m=word2.size();
+        memset(dp,-1,sizeof(dp));
+        return min_operations(word1,word2,n-1,m-1);
+    }
 };
