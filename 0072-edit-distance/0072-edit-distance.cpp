74831cfabcae141126ef439d35064777f2cb9589
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-01 14:21:16 +0800

    Time: 101 ms (8.79%), Space: 7.5 MB (77.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,10 +3,10 @@ public:
     int dp[501][501];
     int min_operations(string &s1,string &s2,int i,int j)
     {
-        if(i<0)return j+1;
-        if(j<0)return i+1;
+        if(i==0)return j;
+        if(j==0)return i;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])
+        if(s1[i-1]==s2[j-1])
         {
             return dp[i][j]=min_operations(s1,s2,i-1,j-1);
         }
@@ -15,6 +15,6 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.size(),m=word2.size();
         memset(dp,-1,sizeof(dp));
-        return min_operations(word1,word2,n-1,m-1);
+        return min_operations(word1,word2,n,m);
     }
 };
