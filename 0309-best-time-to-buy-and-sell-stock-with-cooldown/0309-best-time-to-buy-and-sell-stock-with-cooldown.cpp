bc2afc77343b72ba973d9bc91a0a0c96ebe16774
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-02 15:22:33 +0800

    Time: 0 ms (100.00%), Space: 11.5 MB (69.95%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-int maxProfit(vector<int> &prices) {
-    int buy(INT_MIN), sell(0), prev_sell(0), prev_buy;
-    for (int price : prices) {
-        prev_buy = buy;
-        buy = max(prev_sell - price, buy);
-        prev_sell = sell;
-        sell = max(prev_buy + price, sell);
+    int dp[5001][2];
+    int max_profit(int index,vector<int>&nums,int buy)
+    {
+        if(index>=nums.size())return 0;
+        if(dp[index][buy]!=-1)return dp[index][buy];
+        int profit=INT_MIN;
+        if(buy)
+        {
+            profit=max(-nums[index]+max_profit(index+1,nums,0),max_profit(index+1,nums,1));
+        }
+        else
+        {
+            profit=max(nums[index]+max_profit(index+2,nums,1),max_profit(index+1,nums,0));
+        }
+        return dp[index][buy]=profit;
     }
-    return sell;
-}
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof(dp));
+        return max_profit(0,prices,1);
+    }
 };
