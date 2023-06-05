127fac5842561ff4748c34cc5f8d9354de229a9c
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-05 09:27:38 +0800

    Time: 7 ms (25.98%), Space: 7.6 MB (90.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int dp[101][101];
-    int all_path(int i,int j,vector<vector<int>>grid)
+    int total_path(int i,int j,int m,int n,vector<vector<int>>&grid)
     {
-        if(i>=grid.size() or j>=grid[0].size() or grid[i][j]==1)return 0;
-        if(i==grid.size()-1 and j==grid[0].size()-1)return 1;
+        if(i<0 or i>=m or j<0 or j>=n or grid[i][j]==1)return 0;
+        if(i==m-1 and j==n-1)return 1;
         if(dp[i][j]!=-1)return dp[i][j];
-        int right=all_path(i+1,j,grid);
-        int down=all_path(i,j+1,grid);
-        return dp[i][j]=right+down;
+        return dp[i][j]=total_path(i+1,j,m,n,grid)+total_path(i,j+1,m,n,grid);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        memset(dp, -1, sizeof(dp));
-        return all_path(0,0,grid);
+        memset(dp,-1,sizeof(dp));
+        int m=grid.size(),n=grid[0].size();
+        return total_path(0,0,m,n,grid);
     }
 };
