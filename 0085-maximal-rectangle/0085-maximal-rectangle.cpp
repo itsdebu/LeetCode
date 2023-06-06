de456ca0ec573f8a80cc4f3ceb61cc5cf978649c
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-06 11:19:56 +0800

    Time: 61 ms (43.57%), Space: 17.5 MB (30.28%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int MAH(vector<int>& arr) {
+        vector<int> left, right;
+        stack<int> stk;
+        int n=arr.size();
+        
+		//smallest element in the left
+        for(int i=0;i<n;++i){
+            int cur = arr[i];
+            while(!stk.empty() && arr[stk.top()]>=cur) stk.pop();
+            if(stk.empty()) left.push_back(-1);
+            else left.push_back(stk.top());
+            stk.push(i);
+        }
+        
+        while(!stk.empty()) stk.pop();
+        
+		//smallest element in the right
+        for(int i=n-1;i>=0;--i){
+            int cur = arr[i];
+            while(!stk.empty() && arr[stk.top()]>=cur) stk.pop();
+            if(stk.empty()) right.push_back(n);
+            else right.push_back(stk.top());
+            stk.push(i);
+        }
+        reverse(right.begin(),right.end());
+        
+        int area=0;
+        for(int i=0;i<n;++i){
+            area=max(area, arr[i]*(right[i]-left[i]-1));
+        }
+        return area;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        if(m==0) return 0;
+        int n=matrix[0].size(), result=0;
+        vector<int> histogram(n, 0);
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]=='1')
+                    histogram[j]+=1;
+                else
+                    histogram[j]=0;
+            }
+            
+            result = max(result, MAH(histogram));
+            cout<<result<<" ";
+        }
+        return result;
+    }
+};
