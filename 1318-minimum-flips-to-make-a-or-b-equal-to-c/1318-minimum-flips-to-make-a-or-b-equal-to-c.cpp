92272014b79a1c26ebf49141f973cd8da94c71e1
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-07 14:12:49 +0800

    Time: 0 ms (100.00%), Space: 5.8 MB (93.33%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int count=0;
-        while(a>0 or b>0 or c>0)
-        {
-            int num1=a%2,num2=b%2;
-            if(c%2==0)
-            {
-                if(num1+num2>0)count+=num1+num2;
-            }
-            else
-            {
-                if(num1+num2==0)count+=1;
-            }
-            a/=2,b/=2,c/=2;
-        }return count;
+        int count = 0;
+        for(int i = 1; i <= 32; i++) {
+            int b1 = 0, b2 = 0, b3 = 0;
+            if(((a >> (i - 1)) & 1) >= 1) b1 = 1; // check the ith bit of a
+            if(((b >> (i - 1)) & 1) >= 1) b2 = 1; // check the ith bit of b
+            if(((c >> (i - 1)) & 1) >= 1) b3 = 1; // check the ith bit of c
+            if(b3 == 0 && (b1 == 1 || b2 == 1)) count += b1 + b2; // if the ith bit of c is 0 and any of the ith bits of a or b is 1
+            else if(b3 == 1 && b1 == 0 && b2 == 0) count++; // if the ith bit of c is 1, check the ith bits of a and b
+        }
+        return count;
     }
 };
