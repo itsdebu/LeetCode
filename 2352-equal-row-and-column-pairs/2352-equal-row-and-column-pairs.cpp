d82e2dc41c4a3f59f3502ce7b023472c9688fbd5
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-13 06:55:15 +0530

    Time: 85 ms (98.32%), Space: 25.9 MB (77.48%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int n= grid.size(), count_pairs=0;
-        
-        unordered_map<string,int> mp;
-        
-         //put in the map for each row
-        
+        int n = grid.size();
+
+        map<vector<int>, int> row2cnt;
         for(int i=0;i<n;i++)
-        {
-            string row="";
-            
-            for(int j=0;j<n;j++)
-            {
-                row+= to_string(grid[i][j]) +" ";
-            }
-            
-            mp[row]++;
-        }
+            row2cnt[ grid[i] ]++;
         
-        //collect pairs by storing columns in map
-        
-        for(int i=0;i<n;i++)
-        {
-            string column="";
+        int cnt = 0;
+        vector<int> col(n);
+        for(int j=0;j<n;j++){
+            for(int i=0;i<n;i++)
+                col[i] = grid[i][j];
             
-            for(int j=0;j<n;j++)
-            {
-                column+= to_string(grid[j][i]) +" ";
-            }
-            
-            count_pairs+= mp[column];
+            //NOTE: inserts (key, 0) if not exist
+            cnt += row2cnt[ col ];
         }
-        return count_pairs;
+
+        return cnt;
     }
 };
