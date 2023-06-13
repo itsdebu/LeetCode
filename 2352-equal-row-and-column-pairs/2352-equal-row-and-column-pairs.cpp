bceca17fed218a32e53c170f223faf131d53e17c
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-13 06:54:20 +0530

    Time: 266 ms (35.04%), Space: 44.1 MB (19.47%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int total_count=0;
+        int n= grid.size(), count_pairs=0;
+        
+        unordered_map<string,int> mp;
+        
+         //put in the map for each row
+        
         for(int i=0;i<n;i++)
         {
+            string row="";
+            
             for(int j=0;j<n;j++)
             {
-                int count=0;
-                for(int col=0;col<n;col++)
-                {
-                    if(grid[i][col]!=grid[col][j])
-                    {
-                        count=1;
-                    }
-                }
-                if(count==0)total_count++; 
+                row+= to_string(grid[i][j]) +" ";
             }
-        }return total_count;
+            
+            mp[row]++;
+        }
+        
+        //collect pairs by storing columns in map
+        
+        for(int i=0;i<n;i++)
+        {
+            string column="";
+            
+            for(int j=0;j<n;j++)
+            {
+                column+= to_string(grid[j][i]) +" ";
+            }
+            
+            count_pairs+= mp[column];
+        }
+        return count_pairs;
     }
 };
