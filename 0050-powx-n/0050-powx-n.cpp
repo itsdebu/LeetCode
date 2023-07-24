f8cc3aed5a27b42c63caf8df8d89660ccd1ab7ea
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-07-24 10:20:58 +0800

    Time: 5 ms (6.57%), Space: 5.9 MB (92.36%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1;
+        while (n) {
+            // if (n % 2) res = n > 0 ? res * x : res / x;
+            if(n%2)
+            {
+                if(n>0)
+                    res*=x;
+                else res/=x;
+            }
+            x = x * x;
+            n /= 2;
+        }
+        return res;
+    }
+};
