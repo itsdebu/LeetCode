865fd7e8d4bb326c98afaee97b6f6d8652df8445
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-09-21 10:23:23 +0800

    Time: 2 ms (47.17%), Space: 6.2 MB (27.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int climbStairs(int n) {
+    int dp[45 + 1];
+    int count_steps(int n)
+    {
         if(n<=3)return n;
-        vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=3;
-        for(int i=3;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }return dp[n];
+        if(dp[n]!=-1)return dp[n];
+        return dp[n] =  count_steps(n-1) + count_steps(n-2);
+    }
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        return count_steps(n);
     }
 };
