0e5a576108d48029e4624ede83c31b952d87d551
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-09-25 14:54:20 +0800

    Time: 51 ms (18.17%), Space: 7.8 MB (80.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int dp[501][501];
+int dp[501][501];
+    int min_steps(int index1,int index2,string &s1,string &s2)
+    {
+
+        if(index2<0)return index1+1;
+        if(index1<0)return index2+1;
+
+        if(dp[index1][index2]!=-1)return dp[index1][index2];
+        
+        if(s1[index1]==s2[index2])
+        {
+            return min_steps(index1-1,index2-1,s1,s2);
+        }
+        return dp[index1][index2] = min({1 + min_steps(index1-1,index2,s1,s2),1 + min_steps(index1-1,index2-1,s1,s2), 1 +min_steps(index1,index2-1,s1,s2)});
+
+    }
     int minDistance(string s1, string s2) {
         memset(dp,-1,sizeof(dp));
-        int n=s1.size(),m=s2.size();
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=i;
-        }
-        for(int j=0;j<=m;j++)
-        {
-            dp[0][j]=j;
-        }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s1[i-1]==s2[j-1])
-                {
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
-                }
-                    
-            }
-        }return dp[n][m];
-    }
+        return min_steps(s1.size()-1,s2.size()-1,s1,s2);
+    }   
 };
