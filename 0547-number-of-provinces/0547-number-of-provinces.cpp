006ef14a7c955b494021bcfa38ab18d8320d9174
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-09-29 08:50:30 +0800

    Time: 25 ms (38.97%), Space: 17.3 MB (11.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,79 +1,43 @@
-class DisjointSet {
-    vector<int> rank, parent, size; 
-public: 
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); 
-        parent.resize(n+1);
-        size.resize(n+1); 
-        for(int i = 0;i<=n;i++) {
-            parent[i] = i; 
-            size[i] = 1; 
-        }
-    }
-
-    int findUPar(int node) {
-        if(node == parent[node])
-            return node; 
-        return parent[node] = findUPar(parent[node]); 
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-        }
-        else if(rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u; 
-        }
-        else {
-            parent[ulp_v] = ulp_u; 
-            rank[ulp_u]++; 
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-            size[ulp_v] += size[ulp_u]; 
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v]; 
-        }
-    }
-}; 
 class Solution {
 public:
+    void count_connected(int node,vector<int>adj[],vector<int>&vis)
+    {
+        vis[node] = 1;
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                count_connected(it,adj,vis);
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        DisjointSet ds(n);
+        int m=isConnected[0].size();
+        vector<int>adj[n];
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(isConnected[i][j]==1)
+                if(isConnected[i][j]==1 and i!=j)
                 {
-                    ds.unionBySize(i,j);
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        int count=0;
+        
+        vector<int>vis(n,0);
+        
+        int count = 0;
+
         for(int i=0;i<n;i++)
         {
-            if(ds.findUPar(i)==i)count++;
-        }
-        return count;
+            if(!vis[i])
+            {
+                count++;
+                count_connected(i,adj,vis);
+            }
+        }return count;
     }
 };
-
-
-
-
-
-
-
