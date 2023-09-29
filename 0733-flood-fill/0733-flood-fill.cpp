a5d9bcfdf02eaa811e4f93cfcd10f5fbb10c62ae
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-09-29 09:44:40 +0800

    Time: 3 ms (96.22%), Space: 15 MB (6.03%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<int>>&image,int initial_color,int color,int deltarow[],int deltacol[])
-    {
-        int n=image.size();
-        int m=image[0].size();
-        image[r][c]=color;
-        for(int i=0;i<4;i++)
-        {
-            int new_row=r+deltarow[i];
-            int new_col=c+deltacol[i];
-            if(new_row>=0 and new_row<n and new_col>=0 and new_col<m and image[new_row][new_col]!=color and image[new_row][new_col]==initial_color)
-            {
-                dfs(new_row,new_col,image,initial_color,color,deltarow,deltacol);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+         vector<vector<int>> visited(image.size(),vector<int>(image[0].size(),0));
+         queue<pair<int,int>>q;
+         int n=image.size();
+         int m=image[0].size();
+         q.push({sr,sc});
+         visited[sr][sc]=true;
+        //  return visited;
+        int che=image[sr][sc];
+        int alpha[4]={0,0,1,-1};
+        int beta[4]={1,-1,0,0};
+        while(!q.empty()){
+            pair<int,int>fr=q.front();
+            q.pop();
+            for(int i=0;i<4;i++){
+
+                int x=fr.first+alpha[i];
+                int y=fr.second+beta[i];
+                if(x<n && y<m && x>=0 && y>=0 && visited[x][y]==false){
+                    if(image[x][y]==che){
+                        q.push({x,y});
+                        visited[x][y]=true;
+                        cout<<x<<" "<<y<<endl;
+                    }
+                }
             }
         }
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
-        int initial_color=image[sr][sc];
-        int deltarow[4]={-1,0,1,0};
-        int deltacol[4]={0,1,0,-1};
-        dfs(sr,sc,image,initial_color,color,deltarow,deltacol);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==1){
+                    image[i][j]=color;
+                }
+            }
+        }
+
         return image;
+
     }
 };
