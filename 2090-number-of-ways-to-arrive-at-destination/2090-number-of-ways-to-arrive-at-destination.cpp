caafe958fbf204f189363d73a6b95aed63c8bec9
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-10-02 02:07:19 +0800

    Time: 69 ms (44.15%), Space: 32.9 MB (54.86%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+       vector<pair<int, int>> adj[n];
+        for(auto it : roads){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        // Using Dijkstra's {time, node}
+        priority_queue<pair<long, long>, vector<pair<long, long>>, 
+                    greater<pair<long, long>>> q;
+        q.push({0, 0});
+        
+        vector<long> dist(n, LONG_MAX), ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        int mod = (int)1e9 + 7;
+        while(!q.empty()){
+            int node=q.top().second;
+            long dis=q.top().first;
+            q.pop();
+            for(auto it:adj[node])
+            {
+                int adjnode=it.first;
+                int adjwt=it.second;
+                if(dis+adjwt<dist[adjnode])
+                {
+                    dist[adjnode]=dis+adjwt;
+                    q.push({dist[adjnode],adjnode});
+                    ways[adjnode]=ways[node]%mod;
+                }
+                else if(adjwt+dis==dist[adjnode])
+                {
+                    ways[adjnode] += ways[node];
+                    ways[adjnode] %=mod;
+                }
+            }
+        }return ways[n-1]; 
+    }
+};
