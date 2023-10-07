db15fceda0522a0af1983505f2996988957c5707
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-10-08 00:27:09 +0800

    Time: 161 ms (9.88%), Space: 40.8 MB (7.22%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void unique_permutation(int index,vector<int>nums,set<vector<int>>&st)
+    {
+        if(index == nums.size())
+        {
+            st.insert(nums);
+            return;
+        }
+
+        for(int i = index ; i < nums.size() ; i++ )
+        {
+            swap(nums[index],nums[i]);
+            unique_permutation(index+1,nums,st);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        set<vector<int>>st;
+        unique_permutation(0,nums,st);
+        vector<vector<int>>ans;
+        for(auto it:st)ans.push_back(it);
+        return ans;
+
+    }
+};
