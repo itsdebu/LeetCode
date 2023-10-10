f1b9a53387e680f77c4fc63ff7430e08107c6f5d
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-10-11 00:00:00 +0800

    Time: 152 ms (7.16%), Space: 6.4 MB (17.12%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
+    int dp[31];
     int fib(int n) {
+        memset(dp,-1,sizeof(dp));
         if(n<=1)return n;
 
-        return fib(n-1) + fib(n-2);
+        return dp[n] = fib(n-1) + fib(n-2);
     }
 };
