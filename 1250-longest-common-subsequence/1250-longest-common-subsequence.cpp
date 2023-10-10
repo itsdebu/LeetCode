c74dab9f144b8ecbbfc740d0c0c0965c89ebe085
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-10-11 01:47:13 +0800

    Time: 55 ms (39.19%), Space: 11.5 MB (74.35%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-int dp[1001][1001];
-    int match(int index1,int index2,string &s1,string &s2)
+    int dp[1001][1001];
+    int find_lcs(int index1,int index2,string &s1,string &s2)
     {
         if(index1<0 or index2<0)return 0;
 
-        if(dp[index1][index2]!=-1)return dp[index1][index2];
+        int take = 0,not_take1 = 0,not_take2 = 0;
 
-        int matching = 0,not_match1 = 0,not_match2 = 0;
-        if(s1[index1]==s2[index2])
+        if(dp[index1][index2]!=-1)return dp[index1][index2];
+        if(s1[index1] == s2[index2])
         {
-            matching = 1 + match(index1-1,index2-1,s1,s2);
+            take = 1 + find_lcs(index1-1,index2-1,s1,s2);
         }
-        else {
-            not_match1 = match(index1-1,index2,s1,s2);
-            not_match2 = match(index1,index2-1,s1,s2);
+        else
+        {
+            not_take1 = find_lcs(index1-1,index2,s1,s2);
+            not_take2 = find_lcs(index1,index2-1,s1,s2);
         }
-        return dp[index1][index2] = max({matching,not_match1,not_match2});
+        return dp[index1][index2] = max({not_take1,take,not_take2});
     }
     int longestCommonSubsequence(string text1, string text2) {
         memset(dp,-1,sizeof(dp));
-        return match(text1.size()-1,text2.size()-1,text1,text2);
+        return find_lcs(text1.size()-1,text2.size()-1,text1,text2);
     }
 };
