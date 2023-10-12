c010955d2c98cfdf9c59ff6916d989a5d5c3dac6
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-10-12 12:54:10 +0530

    Time: 208 ms (94.59%), Space: 8.6 MB (89.14%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -3,20 +3,25 @@ public:
     int dp[301][301];
     int max_earn(int i,int j,vector<int>&nums)
     {
-        if(i>j)return 0;
+        if(i>j)return 0;        
+
         if(dp[i][j]!=-1)return dp[i][j];
-        int mx=INT_MIN;
-        for(int ind=i;ind<=j;ind++)
+
+        int mx_coins = INT_MIN;
+
+        for(int k = i;k<=j;k++)
         {
-            int cost=(nums[i-1]*nums[ind]*nums[j+1]) + max_earn(i,ind-1,nums) + max_earn(ind+1,j,nums);
-            mx=max(mx,cost);
-        }return dp[i][j]=mx;
+            int ans = nums[k] * nums[i-1] * nums[j+1] + max_earn(i,k-1,nums) + max_earn(k+1,j,nums);
+            mx_coins = max(mx_coins,ans);
+        }
+        return dp[i][j] = mx_coins;
     }
     int maxCoins(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         memset(dp,-1,sizeof(dp));
         nums.push_back(1);
         nums.insert(nums.begin(),1);
+
         return max_earn(1,n,nums);
     }
 };
