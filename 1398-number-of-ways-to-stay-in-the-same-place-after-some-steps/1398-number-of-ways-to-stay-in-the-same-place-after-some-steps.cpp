74fbe0ca8fd1ebb02db85d8a9878e3d660484e26
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-10-15 22:32:51 +0530

    Time: 29 ms (34.85%), Space: 19.3 MB (22.08%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
+    int MOD = 1e9 + 7;
+    int solveMem(int steps, int arrLen, int idx, vector<vector<long long int>> &dp) {
+        if(idx < 0 || idx >= arrLen || steps < 0) return 0;
+
+        if(idx == 0 && steps == 0) return 1;
+
+        if(dp[steps][idx] != -1) return dp[steps][idx];
+
+        long long int left = solveMem(steps - 1, arrLen, idx - 1, dp) % MOD;
+        long long int right = solveMem(steps - 1, arrLen, idx + 1, dp) % MOD;
+        long long int stay = solveMem(steps - 1, arrLen, idx, dp) % MOD;
+
+        return dp[steps][idx] = (left + stay + right) % MOD;
+    }
     int numWays(int steps, int arrLen) {
-        const int mod = 1000000007;
-        
-        int maxPosition = std::min(steps / 2, arrLen - 1);
-        
-        // Create a 2D vector dp to store the number of ways to reach a specific position at each step.
-        vector<vector<int>> dp(steps + 1, vector<int>(maxPosition + 1, 0));
-        
-        // Initialize the number of ways to stay at position 0 after 0 steps to 1.
-        dp[0][0] = 1;
-        
-        // Loop through the number of steps.
-        for (int i = 1; i <= steps; i++) {
-            for (int j = 0; j <= maxPosition; j++) {
-                // Initialize the number of ways to stay at the current position with the number of ways to stay at the same position in the previous step.
-                dp[i][j] = dp[i - 1][j];
-                
-                // If the current position is greater than 0, add the number of ways to reach it by moving left.
-                if (j > 0) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % mod;
-                }
-                
-                // If the current position is less than the maximum position, add the number of ways to reach it by moving right.
-                if (j < maxPosition) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % mod;
-                }
-            }
-        }
-        
-        // The final result is stored in dp[steps][0], representing the number of ways to reach the initial position after taking 'steps' steps.
-        return dp[steps][0];
+        if(arrLen > steps) arrLen = steps;
+        vector<vector<long long int>> dp(steps + 1, vector<long long int>(arrLen, -1));
+        return solveMem(steps, arrLen, 0, dp);
     }
 };
